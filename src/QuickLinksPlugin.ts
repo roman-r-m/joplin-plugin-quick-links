90286b6802740cd455df743c39bc8660ecb3d894
Roman <musinrr@gmail.com>
2021-01-23 14:35:11 +0000

    1st working prototype

---
+++ src/QuickLinksPlugin.ts
@@ -0,0 +1,74 @@
+module.exports = {
+	default: function(context: any) {
+
+		const buildHints = async (prefix: string) =>{
+			const notes = await context.postMessage({
+                command: 'getNotes',
+                prefix: prefix,
+            });
+
+            let hints = [];
+			for (let i = 0; i < notes.length; i++) {
+				const note = notes[i];
+                hints.push({
+                    text: note.title,
+                    displayText: note.title,
+                    hint: async (cm, data, completion) => {
+                        const from = completion.from || data.from;
+                        from.ch -= 2;
+                        cm.replaceRange(`[${note.title}](:/${note.id})`, from, completion.to || data.to, "complete");
+                    }
+                });
+			}
+			return hints;
+		}
+
+		const plugin = function(CodeMirror) {
+			CodeMirror.defineOption('quickLinks', false, function(cm, value, prev) {
+				if (!value) return;
+
+				cm.on('inputRead', async function (cm1, change) {
+                    if (!cm1.state.completionActive && cm.getTokenAt(cm.getCursor()).string === '@@') {
+                        const start = {line: change.from.line, ch: change.from.ch + 1};
+
+						const hint = function(cm, callback) {
+							const cursor = cm.getCursor();
+							let prefix = cm.getRange(start, cursor) || '';
+
+							buildHints(prefix).then(hints => {
+								callback({
+									list: hints,
+									from: {line: change.from.line, ch: change.from.ch + 1},
+									to: {line: change.to.line, ch: change.to.ch + 1},
+								});
+							});
+						};
+
+						setTimeout(function () {
+							CodeMirror.showHint(cm, hint, {
+								completeSingle: false,
+								closeOnUnfocus: true,
+								async: true,
+							});
+						}, 10);
+					}
+				});
+			});
+		};
+
+		return {
+			plugin: plugin,
+			codeMirrorResources: [
+			    'addon/hint/show-hint',
+			    ],
+			codeMirrorOptions: {
+    			'quickLinks': true,
+			},
+			assets: function() {
+			    return [
+			        { name: './show-hint.css'},
+			    ]
+			}
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,34 @@
 import joplin from 'api';
+import { ContentScriptType, Path } from 'api/types';
 
 joplin.plugins.register({
 	onStart: async function() {
-		console.info('Test plugin started!');
+		await joplin.contentScripts.register(
+			ContentScriptType.CodeMirrorPlugin,
+			'quickLinks',
+			'./QuickLinksPlugin.js'
+		);
+
+		await joplin.contentScripts.onMessage('quickLinks', async (message: any) => {
+			if (message.command === 'getNotes') {
+				const prefix = message.prefix;
+				if (prefix === "") {
+					const notes = await joplin.data.get(['notes'], {
+						fields: ['id', 'title'],
+						order_by: 'updated_time',
+						order_dir: 'DESC',
+						limit: 10,
+					});
+					return notes.items;
+				} else {
+					const notes = await joplin.data.get(['search'], {
+						fields: ['id', 'title'],
+						limit: 10,
+						query: `title:${prefix}*`,
+					});
+					return notes.items;
+				}
+			}
+		});
 	},
 });
