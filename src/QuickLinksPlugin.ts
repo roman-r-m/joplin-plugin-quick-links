e2dc67d315313eadefc9f1284f14c9bbd56d6835
S73ph4n <stephanpissot@gmail.com>
2021-05-23 16:23:43 +0200

    Added wikipedia query

--- src/QuickLinksPlugin.ts
+++ src/QuickLinksPlugin.ts
@@ -38,20 +38,20 @@ module.exports = {
 		}
 
 		const buildHints = async (prefix: string) =>{
-			const response = await context.postMessage({ command: 'getNotes', prefix: prefix });
-
 			let hints: Hint[] = [];
 
+			//get local notes
+			const response = await context.postMessage({ command: 'getNotes', prefix: prefix });
 			const notes = response.notes;
 			for (let i = 0; i < notes.length; i++) {
 				const note = notes[i];
 				const hint: Hint = {
-                    text: note.title,
-                    hint: async (cm, data, completion) => {
-                        const from = completion.from || data.from;
-                        from.ch -= 2;
-                        cm.replaceRange(`[${note.title}](:/${note.id})`, from, cm.getCursor(), "complete");
-					},
+				    text: note.title,
+				    hint: async (cm, data, completion) => {
+					    const from = completion.from || data.from;
+					    from.ch -= 2;
+					    cm.replaceRange(`[${note.title}](:/${note.id})`, from, cm.getCursor(), "complete");
+				    },
 				};
 				if (response.showFolders) {
 					const folder = !!note.folder ? note.folder  : "unknown";
@@ -67,8 +67,49 @@ module.exports = {
 				} else {
 					hint.displayText = note.title;
 				}
-                hints.push(hint);
+				hints.push(hint);
 			}
+
+			//get wikipedia articles
+			if (prefix){
+				//query wikipedia
+				var request = new XMLHttpRequest()
+				request.open('GET', 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srlimit=5&srsearch='+prefix, false)
+				request.send()
+				var data = JSON.parse(request.response)
+				if (request.status >= 200 && request.status < 400) {
+					var results = data.query.search
+					for (let i = 0; i < results.length; i++){
+						var article = results[i]
+						console.log(article.title)
+						const hint0: Hint = {
+							text: article.title,
+							hint: async (cm, data, completion) => {
+								const from = completion.from || data.from;
+								from.ch -= 2;
+								cm.replaceRange(`[${article.title}](https://en.wikipedia.org/wiki/${article.title})`, from, cm.getCursor(), "complete");
+							},
+						};
+						if (response.showFolders) {
+							hint0.render = (elem, _data, _completion) => {
+								const p = elem.ownerDocument.createElement('div');
+								p.setAttribute('style', 'width: 100%; display:table;');
+								elem.appendChild(p);
+								p.innerHTML = `
+								<div style="display:table-cell; padding-right: 5px">${article.title}</div>
+								<div style="display:table-cell; text-align: right;"><small><em>In Wikipedia</em></small></div>
+								`
+							};
+						} else {
+							hint0.displayText = article.title;
+						}
+						hints.push(hint0);
+					}
+				} else {
+					console.log('error')
+				}
+			}
+			
 
 			if(response.allowNewNotes && prefix) {
 				hints.push(NewNoteHint(prefix, false));
